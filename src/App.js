fadc0b6e25f6799757755f218bb5141920acd702
Djamalutin <fisher.gerryy@mail.ru>
2023-05-09 01:21:01 +0300

    feature: creating useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { useState } from "react";
 import "./index.scss";
 function App() {
+  const [count, setCount] = useState(0);
+  const handleCountPlus = () => {
+    setCount(count + 1);
+  };
+  const handleCountMinus = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="App">
       <div>
